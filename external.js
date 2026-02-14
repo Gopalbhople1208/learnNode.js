a7f8a074c4371c715c6f1acd3a1da7fda6f0781a
Gopalbhople <gbhople63@gmail.com>
2026-02-14 22:35:54 +0530

    thsi external package is add file

---
+++ external.js
@@ -0,0 +1,12 @@
+//how to install the external package command(npm i colors) colors name package is install 
+//after is install the package.json ,package-lock.json and node_modules is file is add
+//self add the package.json(node init)
+const colors = require("colors");
+
+console.log(colors.green("this is call the external package"));
+console.log(colors.yellow("this is call the external package"));
+console.log(colors.red("this is call the external package"));
+console.log(colors.blue("this is call the external package"));
+
+
+
