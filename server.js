20c7d104917f76c463d71800f98a7b9d00f0fda0
Gopalbhople <gbhople63@gmail.com>
2026-02-14 19:44:39 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+http.createServer((req, resp) => {
+ resp.write("<h1>this tag add the more text</h1>");
+ resp.write("king the is god of state");
+
+    //end is import to completed the process
+    resp.end("Hello");   // Important to end response
+      
+}).listen(5003);
+
