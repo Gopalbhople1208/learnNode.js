5189133455d4691c00cae4aa881f6f1ea03d87b4
Gopalbhople <gbhople63@gmail.com>
2026-02-18 22:31:26 +0530

    this add created the file and add the data this file

--- curd.js
+++ curd.js
@@ -1,6 +1,7 @@
 
 //created the text file (sync method)
 const fs = require("fs");
+
 //fs.writeFileSync("text/curd1.txt","this is curd operation");
 
 //remove the file(sync method and async method)
@@ -8,8 +9,21 @@ const fs = require("fs");
 
 
 //read the data insaid the file (sync method and async method)
-let data = fs.readFileSync("text/curd.txt","utf-8");
-console.log(data);
+// let data = fs.readFileSync("text/curd.txt","utf-8");
+// console.log(data);
 
 //append the data in the file (sync method and async method)
-const data1 =fs.appendFileSync("text/curd.txt","\nthis checking append file data use of async methode");
+//const data1 =fs.appendFileSync("text/curd.txt","\nthis checking append file data use of async methode");
+ 
+
+//the add the data for direct form the console
+console.log(process.argv[2]);
+const operation = process.argv[2];
+if(operation =='write'){
+    const name = process.argv[3];
+    const data = process.argv[4];
+    const fullname = "text/"+name+".txt";
+
+    fs.writeFileSync(fullname,data);
+    
+}
