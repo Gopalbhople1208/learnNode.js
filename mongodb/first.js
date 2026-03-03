8daa11c80419f7d4fb48484a94b006d0b3b9a6bd
Gopalbhople <gbhople63@gmail.com>
2026-03-03 16:13:26 +0530

    this case the code add correct and the call both and run corrected

--- mongodb/first.js
+++ mongodb/first.js
@@ -9,7 +9,7 @@ const client = new MongoClient(url);
 
 const app = express();
 
-app.set("view engine " ,"ejs");
+app.set("view engine", "ejs");
 app.get('/',async (req,resp)=>{
      await client.connect();
     const db = client.db(dbName);
@@ -17,9 +17,9 @@ app.get('/',async (req,resp)=>{
 
     const result = await collection.find().toArray();
     console.log(result);
-    resp.render("first");
+    resp.render("first",{students:result});
 })
-app.listen(2020,(req,resp)=>{
+app.listen(2020,()=>{
     console.log("This run correctly in http://localhost:2020");
 })
 
