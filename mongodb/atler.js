53839418e38b3024f75884614543de59a01b0867
Gopalbhople <gbhople63@gmail.com>
2026-03-06 15:48:40 +0530

    this created the cookies and get to browser screen

--- mongodb/atler.js
+++ mongodb/atler.js
@@ -1,29 +1,23 @@
 import { MongoClient } from "mongodb";
+const url = "mongodb://cluster0-shard-00-00.wj1kadu.mongodb.net:27017,cluster0-shard-00-01.wj1kadu.mongodb.net:27017,cluster0-shard-00-02.wj1kadu.mongodb.net:27017/?replicaSet=atlas-xyz-shard-0&authSource=admin&retryWrites=true&w=majority";
 
-const url = "mongodb+srv://gbhople63_db_user:GoogleTest@cluster0.wj1kadu.mongodb.net/?retryWrites=true&w=majority";
-const database = "library";
-const collectionName = "books";
+//const url = "mongodb+srv://gbhople63_db_user:GoogleTest@cluster0.wj1kadu.mongodb.net/library?retryWrites=true&w=majority";
 
 const client = new MongoClient(url);
 
-async function dbCollection() {
+async function run() {
   try {
     await client.connect();
-    console.log("✅ MongoDB Connected");
+    console.log("MongoDB Connected");
 
-    const db = client.db(database);
-    const collection = db.collection(collectionName);
+    const db = client.db("library");
+    const books = db.collection("books");
 
-    const result = await collection.find().toArray();
-
-    console.log("📚 Books Data:", result);
-
-  } catch (error) {
-    console.log("❌ Error:", error);
-  } finally {
-    await client.close();
-    console.log("🔌 Connection Closed");
+    const result = await books.find().toArray();
+    console.log(result);
+  } catch (err) {
+    console.log(err);
   }
 }
 
-dbCollection();
+run();
---
+++ mongodb/coockies.js
@@ -0,0 +1,30 @@
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+// Settings
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+// Routes
+app.get('/login', (req, res) => {
+  res.render('login'); // renders views/login.ejs
+});
+
+app.post('/profile', (req, res) => {
+ 
+  res.render('profile'); // renders views/profile.ejs
+});
+
+app.get('/', (req, res) => {
+  const userName = req.cookies.name || 'Guest';
+  console.log('User name from cookie:', userName);
+  res.render('home', { name: userName }); // renders views/home.ejs
+});
+
+// Start server
+app.listen(2030, () => {
+  console.log('Server running at http://localhost:2030');
+});
